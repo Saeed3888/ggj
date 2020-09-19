8e69de4fe01728e698dbaa08293606512c783e83
Saeed5062 <Ssa5468@psu.edu>
2020-09-19 02:20:59 +0000

    980390

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #Author: Saeed Alyami ssa5468@psu.edu
 
-def number_n(n):
+def digit_sum(n):
   n = int(n)
   if n > 0:
-   return n%10 + number_n(n//10)
+   return n%10 + digit_sum(n//10)
   else:
     return n 
 
@@ -16,7 +16,7 @@ def print_n(s,n):
 def run():
   n = input ("Enter an int: ")
   n = int(n)
-  print(f"sum of digits of {n} is {number_n(n)}.")
+  print(f"sum of digits of {n} is {digit_sum(n)}.")
   
 
 if __name__== "__main__":
